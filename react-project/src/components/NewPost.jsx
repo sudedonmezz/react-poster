1385fb2d863a02e32cdce082f5e13bb9ec9d5c51
sudedonmezz <donmezsude41@gmail.com>
2025-10-14 15:12:37 +0300

    handling form submission

--- react-project/src/components/NewPost.jsx
+++ react-project/src/components/NewPost.jsx
@@ -1,22 +1,54 @@
+import { useState } from "react";
 import "./NewPost.css";
+
 
 
 export default function NewPost(props)
 {
  
+  const [enteredBody,setEnteredBody]=useState(''); //return an array
+   const [enteredAuthor,setEnteredAuthor]=useState('');
+ 
+
+  function changeBodyHandler(event)
+  {
+   setEnteredBody(event.target.value); 
+  // document.querySelector(".textarea1").innerHTML=enteredBody;
+  }
+
+  function AuthorChangeHandler(event)
+  {
+    setEnteredAuthor(event.target.value);
+  }
+
+ function submitHandler(event)
+ {
+event.preventDefault();
+const postData={
+  body: enteredBody,
+  author:enteredAuthor
+ };
+ console.log(postData);
+ props.onCancel();
+ }
 
   return (
 
 
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
        <p>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={AuthorChangeHandler} />
       </p>
    
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}></textarea>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}></textarea>
+      </p>
+
+      <p className="actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Submit</button>
       </p>
    
      
--- react-project/src/components/PostsList.jsx
+++ react-project/src/components/PostsList.jsx
@@ -1,6 +1,5 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
-import { useState } from "react";
 import Modal from "./Modal";
 import './Post.css';
 
@@ -8,34 +7,16 @@ import './Post.css';
 export default function PostsList({isPosting,onStopPosting})
 {
 
-   const [enteredBody,setEnteredBody]=useState(''); //return an array
-   const [enteredAuthor,setEnteredAuthor]=useState('');
- 
-
-  function changeBodyListener(event)
-  {
-   setEnteredBody(event.target.value); 
-  // document.querySelector(".textarea1").innerHTML=enteredBody;
-  }
-
-  function AuthorChangeHandler(event)
-  {
-    setEnteredAuthor(event.target.value);
-  }
-
- 
+   
 
   return (
     <>
 
     {isPosting && (<Modal onClose={onStopPosting}>
-    <NewPost onBodyChange={changeBodyListener} onAuthorChange={AuthorChangeHandler}/>
+    <NewPost onCancel={onStopPosting}/>
     </Modal>) }
     
    <ul className="posts">
-     <li> <Post author={enteredAuthor} body={enteredBody}/></li>
-     <li><Post author="tarzzan" body="omg you should try to learn react.js buddy"/></li>
-     <li> <Post author="zayn" body="i started to learn react.js in my village lol"/></li>
       <li><Post author="selena" body="hi there, everyone should learn to use react.js"/></li>
     </ul>
     </>
