6f8f3b295975bdc19d1b5c6a05f03bc1cd4cbf41
sudedonmezz <donmezsude41@gmail.com>
2025-10-15 14:23:22 +0300

    fetched the data from the backend

--- react-project/src/components/PostsList.jsx
+++ react-project/src/components/PostsList.jsx
@@ -2,15 +2,44 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import './Post.css';
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 
 export default function PostsList({isPosting,onStopPosting})
 {
+ 
   const [posts,setPosts]=useState([]);
+  const [isFetching,setIsFetching]=useState(false);
+
+
+  useEffect(()=>{
+   async function fetchPosts()
+    {
+      setIsFetching(true);
+      const response= await fetch('http://localhost:8080/posts');
+      const resData=await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+
+  },[]);
+
+
+
 
   function addPostHandler(postData)
   {
+    fetch('http://localhost:8080/posts',
+      {
+        method:'POST',
+        body: JSON.stringify(postData),
+        headers: {
+          'Content-Type':'application/json'
+        }
+      }
+    );
     setPosts(existingPosts=>[postData, ...existingPosts]);
   }
 
@@ -19,7 +48,7 @@ export default function PostsList({isPosting,onStopPosting})
   return (
     <>
 
-    {isPosting && (<Modal onClose={onStopPosting}>
+    {!isFetching && isPosting && (<Modal onClose={onStopPosting}>
     <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
     </Modal>) }
     {posts.length>0 &&(
@@ -27,12 +56,14 @@ export default function PostsList({isPosting,onStopPosting})
       {posts.map((post)=><Post key={post.body} author={post.author} body={post.body}/>)}
     </ul>
     )}
-    {posts.length===0 && (
+    {!isFetching && posts.length===0 && (
       <div style={{textAlign:'center', color:'peru'} }>
         <h2>There are no posts yet.</h2>
         <p>Start Adding some!</p>
       </div>
     )}
+
+    {isFetching && <div style={{textAlign:'center', color:'peru'} }><p>Loading posts</p></div>}
  
     </>
     );
